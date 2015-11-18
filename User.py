f571cb6640ef18185e54cc1f3e9fc08ba3d10b7a
FedrianHermana <fedrianhermana@gmail.com>
2015-11-18 07:09:44 -0800

    Update User.py

--- User.py
+++ User.py
@@ -1,1 +1,16 @@
-
+class user:
+  def__init__(self,name,computer){
+    self.name=name
+    self.computer=computer
+    if computer==true:
+      self.name="comp"
+    curIdx=1  
+    }
+  def getName(self):
+    return self.name
+  def setCurIdx(self,curIdx):
+    self.curIdx=curIdx
+  def getCurIdx(self):
+    return self.curIdx
+  def isComputer(self):
+    return self.computer
