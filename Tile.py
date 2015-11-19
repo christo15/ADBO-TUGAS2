9e90bc5199bd4dbfef28c5931c207318c9510770
KelvinTandika <kelvintandika@gmail.com>
2015-11-19 20:41:21 +0700

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -1,3 +1,12 @@
 class Tile:
-  def __init__(self)
-  
+  def __init__(self,index):
+    user = user_array[2]
+    self.index = index
+    ulartangga = ulartangga()
+  def setUser(self,user):
+    if self.user[0]==null:
+      self.user[0] = user
+    else if self.user[0] != null:
+      self.user[1] = user
+  def getUlarTangga(self):
+    return self.ulartangga
