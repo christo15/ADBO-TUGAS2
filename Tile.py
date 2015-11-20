0f5686aeb34f6cf11dd17970f22532ad75b039bd
KelvinTandika <kelvintandika@gmail.com>
2015-11-20 14:41:55 +0700

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -1,26 +1,40 @@
-class Tile:
+import UlarTangga
+import User
+
+class Tile():
+    user = User[2]
+    index=0
+    ulartangga = UlarTangga()
   
-  def __init__(self,index):
-    user = user_array[2]
-    self.index = index
-    ulartangga = ulartangga()
+    def __init__(self, index):
+        self.index = index
   
-  def setUser(self,user):
-    if self.user[0]==null:
-      self.user[0] = user
-    else if self.user[0] != null:
-      self.user[1] = user
+    def setuser(self, user):
+        if self.user[0] == null:
+            self.user[0] = user
+        elif self.user[0] != null:
+            self.user[1] = user
   
-  def getUlarTangga(self):
-    return self.ulartangga
+    def getulartangga(self):
+        return self.ulartangga
   
-  def setUlarTangga(self,ulartangga):
-    self.ulartangga=ulartangga
+    def setulartangga(self, ulartangga):
+        self.ulartangga = ulartangga
   
-  def getUser1(self):
-    return self.user[0]
+    def getuser1(self):
+        return self.user[0]
   
-  def getUser2(self):
-    return self.user[1]
-  
-  
+    def getuser2(self):
+        return self.user[1]
+    
+    def getindex(self):
+        print self.index
+        
+    def removeuser(self,nama):
+        if self.user[0]!=null:
+            if self.user[0].getname()==nama:
+                self.user[0]=null
+        elif self.user[1]!=null:
+               if self.user[1].getname()==nama:
+                   self.user[1]=null
+                
