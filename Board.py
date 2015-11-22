83d85af21179c8e7aa68dd5b037e246258179678
KelvinTandika <kelvintandika@gmail.com>
2015-11-22 18:46:12 +0700

    Update Board.py

--- Board.py
+++ Board.py
@@ -1,6 +1,14 @@
 import random
+import Tile
+import User
+import Dice
 
 class Board :
+  tile = Tile[101]
+  player = User[2]
+  dice = Dice()
+  playerTurn = User()
+  rnd = Random()
   def __init__(self,pilihan):
     for x in range(0,101): 
-      tile=
+      tile[x]= Tile(x)
