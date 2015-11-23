2e534681332a876a3bc104d982d93545678a7522
Stillmen Vallian <stillmen.v@gmail.com>
2015-11-23 11:59:37 +0700

    Update Board.py

--- Board.py
+++ Board.py
@@ -9,6 +9,7 @@ class Board :
   dice = Dice()
   playerTurn = User()
   rnd = Random()
+  
   def __init__(self,pilihan):
     for x in range(0,101): 
       tile[x]= Tile(x)
@@ -22,46 +23,79 @@ class Board :
     tile[1].setUser(player[0])
     tile[1].setUser(player[1])
     
-    playerTurn = player[0]
+    self.playerTurn = player[0]
     
     ##Snake and ladder
     for x in range(0,5):
       ##Snake
-      idx = randrange(2, 97)
-      idxTo = random
+      idx = random.randint(2, 97)
+      idxTo = random.randint(0, idx-2)
+      if  tile[idx].getUlarTangga == None:
+        tile[idx].setUlarTangga(UlarTangga(idxTo))
       
+      ##Ladder
+      idx2 = random.randint(2, 97)
+      idxTo2 = random.randint(0, 98-idx2) + idx2
+      if  tile[idx2].getUlarTangga == None:
+        tile[idx2].setUlarTangga(UlarTangga(idxTo2))
+  
+  
+  def run(self,x):
+    if  x == 1:
+      temp=playerTurn.getCurIdx + dice.roll
+      if  temp <= 100:
+        temp=checkAndChange(temp)
+        tile[playerTurn.getCurIdx].removeUser(playerTurn.getName)
+        tile[temp].setUser(playerTurn)
+        playerTurn.setCurIdx(temp)
+      else:
+        tile[playerTurn.getCurIdx()].removeUser(playerTurn.getName());
+        temp = temp - 100;
+        temp = 100 - temp;
+        temp = self.checkAndChange(temp);
+        tile[temp].setUser(playerTurn);
+        playerTurn.setCurIdx(temp);
+    else:
+      sys.exit(0)
       
+    self.changeTurn
+  
+  def checkAndChange(self, temp):
+    if  tile[temp].getUlarTangga != None:
+      return tile[temp].getUlarTangga.getToIndex
+    return temp
+  
+  def changeTurn(self):
+    if playerTurn == player[0]:
+      self.playerTurn = player[1]
+    elif  playerTurn == player[1]:
+      self.playerTurn = player[0]
+  
+  def printMap(self):
+    for x in range(1,100):
+      if  x < 10:
+        print i+"  "
+      elif  x >= 10:
+        print i+" "
+      elif  x == 100:
+        print i+""
       
+      if  tile[x].getUser1 == None & tile[x].getUser2 == None:
+        print "   -  "
+      else:
+        if  tile[x].getUser1 != None:
+          print tile[x].getUser1.getName + " "
+        if  tile[x].getUser2 != None:
+          print tile[x].getUser2.getName + " "
       
+      if  tile[x].getUlarTangga != null:
+        if  x < 10:
+          print tile[x].getUlarTangga.getToIndex + "   "
+        elif  x > 9:
+          print tile[x].getUlarTangga.getToIndex + " "
+      elif  tile[x].getUlarTangga == None:
+        print "   "
+      
+      if  x % 5 == 0:
+        print "\n"
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  ## 139
-    def getPlayer(self):
-      return self.player
-    
-    def getPlayerTurn(self):
-      return playerTurn
-    
-    def isWin(self):
-      if player[0].getCurIdx == 100 ^| player[1].getCurIdx == 100:
-        if player[0].getCurIdx == 100:
-          print player[0].getName + " is The Winner"
-        else:
-          print player[1].getName + " is The Winner"
-        print "=================================CONGRATULATION!!!=================================="
-        return true
-      return false
-        
