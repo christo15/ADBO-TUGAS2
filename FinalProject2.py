f206db21eb85c0007ddfd876882e4a05408368d0
KelvinTandika <kelvintandika@gmail.com>
2015-11-24 16:57:42 +0700

    Update FinalProject2.py

--- FinalProject2.py
+++ FinalProject2.py
@@ -31,3 +31,46 @@ class User:
   def isComputer(self):
     return self.computer
 
+class UlarTangga(object):
+	def __init__(self, toIndex):
+		self._toIndex = toIndex
+
+	def getToIndex(self):
+		return self._toIndex
+
+
+
+class Tile():
+    index=0
+    user = []
+    ulartangga = None
+    def __init__(self, index):
+        self.index = index
+  
+    def seterUser(self, User):
+        if len(self.user)<2:
+            self.user.append(User)
+  
+    def getulartangga(self):
+        return self.ulartangga
+  
+    def setulartangga(self, ulartangga):
+        self.ulartangga = ulartangga
+  
+    def getuser1(self):
+        return self.user[0]
+  
+    def getuser2(self):
+        return self.user[1]
+    
+    def getindex(self):
+        print (self.index)
+        
+    def removeuser(self,nama):
+        if self.user[0]:
+            if self.user[0].getName()==nama:
+                self.user.remove(self.user[0])
+        elif self.user[1]:
+               print("hskdhflaskdfasdflhsdfhalsdhf")
+               if self.user[1].getName()==nama:
+                   self.user[1].remove(self.user[1])
